a6d18b9dea8d03a3572e2650d8bad684632e936d
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-10-21 21:45:57 +0900

    fix dependencies
    # Conflicts:
    #	Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/coenttb/swift-structured-queries-postgres",
-            from: "0.2.0",
+            branch: "main",
             traits: ["StructuredQueriesPostgresTagged"]
         ),
         .package(url: "https://github.com/vapor/postgres-nio", from: "1.21.0"),
